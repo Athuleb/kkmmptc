cc8293f6fd4e928054db96980804ba8a4d4724ba
Athuleb <athulbalaneb@gmail.com>
2025-05-28 18:31:05 +0530

    add only kkmmptc-dist.zip

--- kkmmptc/src/pages/PageNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import notFoundImage from '../assets/images/404page.webp';
-function PageNotFound() {
-  return (
-    <div>PageNotFound</div>
-  )
-}
-
-export default PageNotFound
